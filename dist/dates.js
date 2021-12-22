25b34f96a82bca11670470db8b4ee7e49095ff4f
merlinjones10 <merlinjones10@gmail.com>
2021-12-22 14:42:12 +0000

    working function to get days invested in high and low periods of investment

--- dist/dates.js
+++ dist/dates.js
@@ -1,45 +1,48 @@
 "use strict";
 const differenceInCalendarDays = require('date-fns/differenceInCalendarDays');
 const getOverlappingDaysInIntervals = require('date-fns/getOverlappingDaysInIntervals');
-const differenceInCalendarYears = require('date-fns/differenceInCalendarYears');
+const eachYearOfInterval = require('date-fns/eachYearOfInterval');
+const addYears = require('date-fns/addYears');
+const getYear = require('date-fns/getYear');
 class Interval {
     constructor(start, end) {
         this.start = start;
         this.end = end;
     }
     years() {
-        return differenceInCalendarYears(this.end, this.start);
+        return eachYearOfInterval({ start: this.start, end: this.end });
     }
 }
 // (Start, End)
 const lowInterestPeriod = new Interval(new Date(2021, 0, 1), new Date(2021, 3, 20));
 const highInterestPeriod = new Interval(new Date(2021, 3, 21), new Date(2021, 11, 30));
 // Invest from the 1st of April to 10th of May
-const investedDates = new Interval(new Date(2021, 3, 1), new Date(2021, 4, 10));
-//
-const lowInterestDays = getOverlappingDaysInIntervals({ start: lowInterestPeriod.start, end: lowInterestPeriod.end }, { start: investedDates.start, end: investedDates.end });
+const investedDates = new Interval(new Date(2021, 3, 1), new Date(2023, 4, 10));
+// LOOP to create all possible low interest periods
+const allLowInterestPeriods = [];
+for (let i = 0; i < investedDates.years().length; i++) {
+    let year = getYear(investedDates.years()[i]);
+    // BUG, Check month formatting. adding one to the month?
+    let lowInterestPeriod = new Interval(new Date(year, 0, 1), new Date(year, 3, 20));
+    allLowInterestPeriods.push(lowInterestPeriod);
+}
 const daysInvested = (investedDates) => {
-    const lowInterestDays = getOverlappingDaysInIntervals({ start: lowInterestPeriod.start, end: lowInterestPeriod.end }, { start: investedDates.start, end: investedDates.end });
+    let lowInterestTotalDays = 0;
+    for (let i = 0; i < allLowInterestPeriods.length; i++) {
+        lowInterestTotalDays += getOverlappingDaysInIntervals({ start: allLowInterestPeriods[i].start, end: allLowInterestPeriods[i].end }, { start: investedDates.start, end: investedDates.end });
+    }
     const totalDays = differenceInCalendarDays(investedDates.end, investedDates.start);
-    const highInterestDays = totalDays - lowInterestDays;
-    return { total: totalDays, hi: highInterestDays, lo: lowInterestDays };
+    const highInterestDays = totalDays - lowInterestTotalDays;
+    return { total: totalDays, hi: highInterestDays, lo: lowInterestTotalDays };
 };
-console.log(daysInvested(investedDates));
+let totalStats = daysInvested(investedDates);
+console.log(totalStats);
 module.exports = daysInvested;
 // PLAN
 /* ------------------------ // Get input from a user ------------------------ */
 // Format input to Date type
 // GET distance betweetn the two dates in calendar days
 // Create an interval that represents the different interest periods, possibly over a number of years
-// Get an array of ALL DATES between the input dates
-// LOOP through the array, if it matches a date within the specified period + 1 to low, if not +1 to high
+// get total possible years and create a loop to create all possible intervals of low interest days
 // Low and high should equal initial distance (above)
 // Use low and high as 'TIME' in the calc function then + together
-// const readline = require('readline').createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
-// readline.question(`What's your name?`, (name: string) => {
-//   console.log(`Hi ${name}!`);
-//   readline.close();
-// })

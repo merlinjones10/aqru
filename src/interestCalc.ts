bd2562772e24bf985e3e9bf31c1b670f8d81adbb
merlinjones10 <merlinjones10@gmail.com>
2021-12-22 12:57:33 +0000

    refactored to calc to accept interest rate

--- src/interestCalc.ts
+++ src/interestCalc.ts
@@ -1,27 +1,22 @@
-// Static retun on investment. No changing rates
-const r: number = 12 / 100;
-
 const roundNumber2DecPlace = (amount: number): number => {
   return Math.round(amount * 100) / 100;
 };
 
-const totalAccruedAmount = (p: number, t: number): number => {
+const totalAccruedAmount = (p: number, t: number, i: number): number => {
+  // interest amount in decimal form
+  const interest = i / 100;
   // convert time to years
   let days: number = t / 365;
-  let amount = p * (1 + r * days);
+  let amount = p * (1 + interest * days);
   const rounded = roundNumber2DecPlace(amount);
   return rounded;
 };
 
-const testAmount = totalAccruedAmount(20, 345);
+const testAmount = totalAccruedAmount(20, 345, 12);
 console.log(testAmount);
 
 module.exports = totalAccruedAmount;
 
-// A = P(1 + rt)
-// A = Total Accrued Amount (principal + interest)
-// P = Principal Amount
-// I = Interest Amount
-// r = Rate of Interest per year in decimal; r = R/100
-// R = Rate of Interest per year as a percent; R = r \* 100
-// t = Time Period involved in months or years. t = (days/365)
+// p = Principal Amount
+// i = Interest Amount (non decimal form)
+// t = time in days
--- tests/sum.test.js
+++ tests/sum.test.js
@@ -3,9 +3,9 @@ const totalAccruedAmount = require('../dist/interestCalc');
 
 // CLOSE TO to avoid tiny rounding errors
 test('gets total return from amount invested', () => {
-  expect(totalAccruedAmount(1000, 100)).toBeCloseTo(1032.88);
+  expect(totalAccruedAmount(1000, 100, 12)).toBeCloseTo(1032.88);
 });
 
 test('gets total return from amount invested', () => {
-  expect(totalAccruedAmount(20, 345)).toBeCloseTo(22.27);
+  expect(totalAccruedAmount(20, 345, 13.5)).toBeCloseTo(22.55);
 });
